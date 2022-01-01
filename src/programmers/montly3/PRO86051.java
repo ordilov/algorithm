a452bf48bbf2341a10b561d69970dad49d9186cc
ordilov <ordilov@gmail.com>
2022-01-01 14:35:24 +0900

    [프로그래머스] 없는 숫자 더하기 (월간 코드 챌린지 시즌3)

---
+++ src/programmers/montly3/PRO86051.java
@@ -0,0 +1,18 @@
+package programmers.montly3;
+
+// [없는 숫자 더하기] https://programmers.co.kr/learn/courses/30/lessons/86051?language=java
+public class PRO86051 {
+
+    public int solution(int[] numbers) {
+        /*
+        0~9 까지 더한 값은 45이다.
+        45에서 numbers 를 더한 값을 뺴면 나머지를 더한 값이다.
+        */
+        int maxSum = 45;
+        int sum = 0;
+        for(int number: numbers)
+            sum += number;
+        return maxSum - sum;
+    }
+
+}
