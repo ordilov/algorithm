0a50d21c05f245ebd226ed29fbb163e019228c9b
ordilov <ordilov@gmail.com>
2022-01-26 00:15:56 +0900

    [백준] ??!

---
+++ src/boj/operation/BOJ10926.java
@@ -0,0 +1,15 @@
+package boj.operation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//[??!] https://www.acmicpc.net/problem/10926
+public class BOJ10926 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+        System.out.println(id + "??!");
+    }
+}
