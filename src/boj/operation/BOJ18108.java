8ac7f7aa45b4dae9288a355655180ed3e3a9cbed
ordilov <ordilov@gmail.com>
2022-01-26 00:18:12 +0900

    [백준] 1998

---
+++ src/boj/operation/BOJ18108.java
@@ -0,0 +1,14 @@
+package boj.operation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ18108 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(y - 543);
+    }
+}
