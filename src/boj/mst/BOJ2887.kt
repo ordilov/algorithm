de80342d54c1a23bde3391c0560255353add5181
ordilov <ordilov@gmail.com>
2022-09-23 17:31:08 +0900

    [백준] 행성 터널

--- src/boj/mst/BOJ2887.kt
+++ src/boj/mst/BOJ2887.kt
@@ -3,17 +3,16 @@ package boj.mst
 import java.util.*
 
 //[행성 터널] https://www.acmicpc.net/problem/2887
-var parent: IntArray = intArrayOf()
-var rank: IntArray = intArrayOf()
-
-fun main() {
-    val n = readLine()!!.toInt()
-    val xQueue: MutableList<Pair<Int, Int>> = mutableListOf()
-    val yQueue: MutableList<Pair<Int, Int>> = mutableListOf()
-    val zQueue: MutableList<Pair<Int, Int>> = mutableListOf()
+private lateinit var parent: IntArray
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val xQueue = ArrayList<Pair<Int, Int>>(n)
+    val yQueue = ArrayList<Pair<Int, Int>>(n)
+    val zQueue = ArrayList<Pair<Int, Int>>(n)
+    parent = IntArray(n) { it }
 
     repeat(n) {
-        val (x, y, z) = readLine()!!.split(" ").map { it.toInt() }
+        val (x, y, z) = readLine().split(" ").map { i -> i.toInt() }
         xQueue.add(Pair(it, x))
         yQueue.add(Pair(it, y))
         zQueue.add(Pair(it, z))
@@ -22,35 +21,17 @@ fun main() {
     yQueue.sortBy { it.second }
     zQueue.sortBy { it.second }
 
-    val queue: PriorityQueue<Pair<Pair<Int, Int>, Int>> = PriorityQueue(compareBy { it.second })
+    val queue = PriorityQueue<Triple<Int, Int, Int>>(compareBy { it.third })
     for (i in 0 until n - 1) {
-        queue.add(
-            Pair(
-                Pair(xQueue[i].first, xQueue[i + 1].first),
-                xQueue[i + 1].second - xQueue[i].second
-            )
-        )
-        queue.add(
-            Pair(
-                Pair(yQueue[i].first, yQueue[i + 1].first),
-                yQueue[i + 1].second - yQueue[i].second
-            )
-        )
-        queue.add(
-            Pair(
-                Pair(zQueue[i].first, zQueue[i + 1].first),
-                zQueue[i + 1].second - zQueue[i].second
-            )
-        )
+        queue.offer(Triple(xQueue[i].first, xQueue[i + 1].first, xQueue[i + 1].second - xQueue[i].second))
+        queue.offer(Triple(yQueue[i].first, yQueue[i + 1].first, yQueue[i + 1].second - yQueue[i].second))
+        queue.offer(Triple(zQueue[i].first, zQueue[i + 1].first, zQueue[i + 1].second - zQueue[i].second))
     }
 
     var result = 0
-    parent = IntArray(n) { it }
-    rank = IntArray(n) { 1 }
     while (queue.isNotEmpty()) {
-        val (loc, distance) = queue.poll()
-        val (a, b) = loc
-        val connect = union(a, b)
+        val (from, to, distance) = queue.poll()
+        val connect = union(from, to)
         if (connect) result += distance
     }
     println(result)
@@ -60,18 +41,13 @@ private fun union(a: Int, b: Int): Boolean {
     val aRoot = find(a)
     val bRoot = find(b)
     if (aRoot == bRoot) return false
-    if (rank[aRoot] < rank[bRoot]) {
-        parent[aRoot] = bRoot
-    } else {
-        parent[bRoot] = aRoot
-        if (rank[aRoot] == rank[bRoot]) rank[aRoot]++
-    }
+    parent[bRoot] = aRoot
     return true
 }
 
-private fun find(a: Int): Int {
-    if (parent[a] == a) return a
-    val findParent = find(parent[a])
-    parent[a] = findParent
+private fun find(child: Int): Int {
+    if (parent[child] == child) return child
+    val findParent = find(parent[child])
+    parent[child] = findParent
     return findParent
 }
